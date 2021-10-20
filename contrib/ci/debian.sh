0b702b825fa64f875c23f9cc44f0fe2821a81408
Richard Hughes <richard@hughsie.com>
2021-10-20 14:40:47 +0100

    f

--- contrib/ci/debian.sh
+++ contrib/ci/debian.sh
@@ -79,6 +79,10 @@ fi
 PACKAGES=$(find .. -type f -name "*.deb" | grep -v 'fwupd-tests\|dbgsym')
 dpkg -i $PACKAGES
 
+# copy in more non-generated data
+mkdir -p /usr/share/installed-tests/fwupd
+cp fwupd-test-firmware/installed-tests/* /usr/share/installed-tests/fwupd/ -Rv
+
 # run the installed tests
 if [ "$CI" = "true" ]; then
 	dpkg -i ../fwupd-tests*.deb
